0a9f3ce194dda04b9e732bf4420aad14d62e4d69
darthmiku <darthmiku@hotmail.com>
2017-02-22 17:40:59 +0000

    discos

--- discos/models.py
+++ discos/models.py
@@ -14,7 +14,7 @@ class Oferta_disc (models.Model):
     preu=models.IntegerField()
     estat = models.CharField( max_length=20,default="",)
     descripcio= models.CharField( max_length=300, default="", blank=True)
-    data_pujada=models.DateField()
+    data_pujada=models.DateField( auto_now_add=True)
     usuari_venedor=models.ForeignKey('usuaris.Perfil',on_delete=models.SET_NULL,blank=True, null=True,related_name="discos_oferta")
     usuari_comprador=models.ForeignKey('usuaris.Perfil',on_delete=models.SET_NULL,blank=True, null=True, related_name="discos_comprats")
     venut=models.BooleanField(default=False, editable=False)
