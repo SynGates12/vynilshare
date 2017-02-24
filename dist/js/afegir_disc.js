e61d0e8b22987aab61b28214bfde36df4f5c02d9
raoh <albertredal@gmail.com>
2017-02-24 16:27:14 +0000

    Previsualitzacions

--- dist/js/afegir_disc.js
+++ dist/js/afegir_disc.js
@@ -1,5 +1,16 @@
-var loadFile = function(event) {
-                var output = document.getElementById('output');
-                output.src = URL.createObjectURL(event.target.files[0]);
-              };
-              
+$(document).ready(function(){
+function mostrarImagen(input) {
+         if (input.files && input.files[0]) {
+        alert("HOLA")
+          var reader = new FileReader();
+          reader.onload = function (e) {
+           $('#img-user').css('backgroundImage','url('+e.target.result+')');
+          }
+          reader.readAsDataURL(input.files[0]);
+         }
+    }
+
+        $("#id_image").change(function(){
+        mostrarImagen(this);
+    });
+})
