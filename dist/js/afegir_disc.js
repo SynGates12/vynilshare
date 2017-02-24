403ef305a68b39d0d8e0e6b98e0fc39aa44c2ef1
Oriol Julia Brunet <ojulia14@gmail.com>
2017-02-24 18:11:53 +0000

    merge afegir arreglat

--- dist/js/afegir_disc.js
+++ dist/js/afegir_disc.js
@@ -1,16 +1,12 @@
 $(document).ready(function(){
 function mostrarImagen(input) {
          if (input.files && input.files[0]) {
-<<<<<<< HEAD
+
         
           var reader = new FileReader();
           reader.onload = function (e) {
            $('#img-dsk').css('backgroundImage','url('+e.target.result+')');
-=======
-          var reader = new FileReader();
-          reader.onload = function (e) {
-           $('#img-usr').css('backgroundImage','url('+e.target.result+')');
->>>>>>> c613c034a3da29c1d2ea307672ac107224b450fd
+
           }
           reader.readAsDataURL(input.files[0]);
          }
